614cf7b33c3665c882da8957fc605e9120f89569
Eve Porcello <eve@moonhighway.com>
2019-01-09 12:37:35 -0800

    Update index.js

--- lesson-15/server/index.js
+++ lesson-15/server/index.js
@@ -4,17 +4,17 @@ const { GraphQLScalarType } = require("graphql");
 
 const typeDefs = `
     "A scalar type for parsing and serializing dates"
-    scalar DateTime
+    scalar Date
 
     "An object that describes the characteristics of a ski day"
     type SkiDay {
         "A ski day's unique ID"
         id: ID!
         "The date that this ski day occurred"
-        date: DateTime!
-        "The resort where this ski day took place"
-        resort: String!
-        "The conditions at the resort on this particular ski day"
+        date: Date!
+        "The mountain where this ski day took place"
+        mountain: String!
+        "The conditions at the mountain on this particular ski day"
         conditions: Conditions
     }
 
@@ -39,7 +39,7 @@ const typeDefs = `
 
     type Mutation {
         "Adds a day to a skier's total number of ski days during a season"
-        addDay(date: DateTime! resort: String! conditions: Conditions): SkiDay
+        addDay(date: Date! mountain: String! conditions: Conditions): SkiDay
         "Removes a day from a skier's total number of ski days during a season"
         removeDay: Int
     }
@@ -49,19 +49,19 @@ let skiDays = [
   {
     id: "2WEKaVNO",
     date: "3/28/2019",
-    resort: "Kirkwood",
+    mountain: "Mt. Tallac",
     conditions: "POWDER"
   },
   {
     id: "hwX6aOr7",
     date: "1/2/2019",
-    resort: "Jackson Hole",
+    mountain: "Freel Peak",
     conditions: "POWDER"
   },
   {
     id: "a4vhAoFG",
     date: "11/23/2019",
-    resort: "Alpine Meadows",
+    mountain: "Tamarack Peak",
     conditions: "ICE"
   }
 ];
@@ -72,14 +72,14 @@ const resolvers = {
     allDays: () => skiDays
   },
   Mutation: {
-    addDay: (parent, { date, resort, conditions }) => {
-      if (resort === "") {
-        throw new Error("The name of a ski resort must be provided");
+    addDay: (parent, { date, mountain, conditions }) => {
+      if (mountain === "") {
+        throw new Error("The name of a mountain must be provided");
       }
       let newDay = {
         id: generate(),
         date,
-        resort,
+        mountain,
         conditions
       };
       skiDays = [...skiDays, newDay];
@@ -87,8 +87,8 @@ const resolvers = {
     },
     removeDay: () => --skiDays
   },
-  DateTime: new GraphQLScalarType({
-    name: "DateTime",
+  Date: new GraphQLScalarType({
+    name: "Date",
     description: "A valid date time value.",
     parseValue: value => new Date(value),
     serialize: value => new Date(value).toISOString().substring(0, 10),
